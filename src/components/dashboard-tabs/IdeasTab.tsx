d81f9c566768cd174d50e1cb2fc6d587d679384c
bigimpactkartik <bigimpact.kartik@gmail.com>
2025-06-26 11:22:22 +0530

    Updated IdeasTab.tsx

--- src/components/dashboard-tabs/IdeasTab.tsx
+++ src/components/dashboard-tabs/IdeasTab.tsx
@@ -47,9 +47,9 @@ const IdeasTab = () => {
 
   // Webhook URLs for different platforms
   const webhookUrls = {
-    LinkedIn: "https://hook.eu2.make.com/xaikyfjrn4tbhuut7klil7e3f2slqt8w",
+    LinkedIn: "https://hook.eu2.make.com/uo1mv8iqh5dnljhodml75kppe8e6j1fy",
     Instagram: "https://hook.eu2.make.com/jd7mofm8w2622mjadxlya9w481f7rboq",
-    Facebook: "https://hook.eu2.make.com/2fx3hwsl626vxuefc6g8jkbnwqn6wvje"
+    Facebook: "https://hook.eu2.make.com/eeia98ktnu8vth9epzmv89ym4a6eeyi3"
   };
 
   const handleRowClick = (ideaId: number) => {
